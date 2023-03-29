1de9bf75d22b775a3ac7bfa82b68cfbcca3f525b
GitHub Action <action@github.com>
2023-03-29 06:08:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/icodeboxfox/whiteboard/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/icodeboxfox/whiteboard/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.icodeboxfox.whiteboard.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
